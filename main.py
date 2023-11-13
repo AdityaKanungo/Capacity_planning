4bf205d9108517a70e12bded2aa6aeec4cdc6ee9
Aditya Kanungo <aditya.kanungo@gmail.com>
2023-11-13 14:16:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-# Install the necessary packages before running this script:
-# pip install streamlit python-docx openai
-
+# Import necessary libraries
 import streamlit as st
-from docx import Document
+import docx2txt
 import openai
 
-# Replace 'your-api-key' with your actual OpenAI API key
+# Your OpenAI API key (replace with your actual key)
 openai.api_key = 'your-api-key'
 
-def read_docx(file):
-    """Read the content of a docx file and return it as plain text."""
-    doc = Document(file)
-    full_text = []
-    for para in doc.paragraphs:
-        full_text.append(para.text)
-    return '\n'.join(full_text)
+def extract_text_from_docx(file):
+    """Extract text from a DOCX file using docx2txt."""
+    return docx2txt.process(file)
 
-def ask_gpt3(question, document_content, rules):
-    """Use GPT-3 to answer a question based on the document content and rules."""
-    prompt = f"{rules}\n\n{document_content}\n\nQuestion: {question}\nAnswer:"
+def ask_gpt3(question, document_text):
+    """Use GPT-3 to answer a question based on the document content."""
+    # Here you could add your rules or additional instructions for GPT-3.
+    prompt = f"Document: {document_text}\n\nQuestion: {question}\nAnswer:"
     
     response = openai.Completion.create(
         engine="text-davinci-003",
@@ -27,25 +22,20 @@ def ask_gpt3(question, document_content, rules):
     )
     return response.choices[0].text.strip()
 
-st.title('Word Document QA with Rules')
+# Streamlit app interface
+st.title('Word Document Interaction App')
 
 # File uploader to accept Word documents
-uploaded_file = st.file_uploader("Upload a Word document", type=["docx"])
+uploaded_file = st.file_uploader("Upload a Word document", type="docx")
 if uploaded_file is not None:
-    # Read the document content
-    document_content = read_docx(uploaded_file)
-    # Show the document content (optional, could be removed for large documents)
-    st.text_area("Document Content", value=document_content, height=300, disabled=True)
+    # Extract the document text
+    document_text = extract_text_from_docx(uploaded_file)
+    st.text_area("Document Content", value=document_text, height=300, disabled=True)
     
-    # Input for rules on how to answer questions
-    rules = st.text_area("Rules for answering questions",
-                         value="The answer should be concise and to the point.",
-                         height=100)
-    
-    # Input for asking questions
+    # User input for asking questions
     question = st.text_input("Ask a question based on the document")
     
     if question:
         # Generate an answer using GPT-3
-        answer = ask_gpt3(question, document_content, rules)
+        answer = ask_gpt3(question, document_text)
         st.write(answer)
